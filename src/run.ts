d5759fa7e331f39b9fdd4fcf14d7bf43156a198f
Andrey <priv1984@yandex.ru>
2023-02-10 01:01:04 +0700

    Initial commit

---
+++ src/run.ts
@@ -0,0 +1,11 @@
+import { IExecutor } from './Executor';
+import ITask from './Task';
+
+export default async function run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
+    maxThreads = Math.max(0, maxThreads);
+    /**
+     * Код надо писать сюда
+     * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
+     */
+
+}
