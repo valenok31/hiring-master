943c1e5a26fb51e1cc7c96a6afaf7c311ad84ee8
Andrey <priv1984@yandex.ru>
2023-02-12 23:02:23 +0700

    test

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,4 @@
-import { IExecutor } from './Executor';
+import {IExecutor} from './Executor';
 import ITask from './Task';
 
 export default async function run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
@@ -8,7 +8,9 @@ export default async function run(executor: IExecutor, queue: AsyncIterable<ITas
      * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
      */
 
-    console.log('тесты ' + executor.executeTask({ targetId: 0, action: 'init' }))
-    // executor.executeTask({ targetId: 0, action: 'init' });
+    console.log('queue'); // 1,2,3,4,5
+    console.log(JSON.stringify(queue)); // 1,2,3,4,5
+
+
 
 }
