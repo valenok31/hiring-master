b7dbf6b90d8fb471b3afd1d8a7dd1e42e83710ab
Andrey <priv1984@yandex.ru>
2023-02-15 00:07:44 +0700

    for await

--- src/run.ts
+++ src/run.ts
@@ -7,10 +7,24 @@ export default async function run(executor: IExecutor, queue: AsyncIterable<ITas
      * Код надо писать сюда
      * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
      */
-    let arr = [];
+    const arr: any = [];
+    let thread: number = 0;
     for await (const line of queue) {
-        //console.log(line);
-        await executor.executeTask(line);
 
+        //arr[i] = await executor.executeTask(line);
+        if (arr.includes(line.targetId)) {
+            await executor.executeTask(line);
+            thread <= 0 ? thread = 0 : thread--;
+        } else {
+            if (thread <= maxThreads) {
+                executor.executeTask(line);
+                arr.push(line.targetId);
+                thread++;
+            } else {
+                await executor.executeTask(line);
+            }
+
+        }
     }
+    console.log(arr);
 }
