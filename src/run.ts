541230392cb781c7e8e64a0b70f13910a1fa96fa
Andrey <priv1984@yandex.ru>
2023-02-20 18:58:47 +0700

    fatal error 50%

--- src/run.ts
+++ src/run.ts
@@ -3,6 +3,9 @@ import ITask from './Task';
 
 export default async function run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
     maxThreads = Math.max(0, maxThreads);
+    if (maxThreads === 0) {
+        maxThreads = 1000;
+    }
     /**
      * Код надо писать сюда
      * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
@@ -10,51 +13,34 @@ export default async function run(executor: IExecutor, queue: AsyncIterable<ITas
 
     const arr: any = [];
     const arrTask: any = [];
+    const arrTaskRunning: any = [];
     let taskNext = JSON.parse(JSON.stringify(queue));
     let bool = true;
     let n = 0;
+    let threads = 0;
 
     for (let i of taskNext.q) {
         arr.push(i.targetId);
     }
-
+    console.log(maxThreads);
 
     for await (let task of queue) {
-
-        if (true || (arrTask.includes(arr[n + 1]) || arrTask.includes(arr[n])) || arrTask.length > 1) {
+        arrTaskRunning.push(task.targetId);
+        if (arr[n + 1] === undefined || arr[n] === undefined) {
             await executor.executeTask(task);
-        } else {
-            /*            if (maxThreads === 0) {
-                            executor.executeTask(task);
-                        } else {
-                            if (arrTask.length < maxThreads) {
-                                executor.executeTask(task);
-                            } else {
-                                await executor.executeTask(task);
-                            }
-                        }*/
-            executor.executeTask(task);
-
+            continue;
         }
-
-        /*     if (bool && arr[n] != arr[n + 1] && arr[n + 1] != arr[n + 2] && arr[n] != arr[n + 2]) {
-                 executor.executeTask(task);
-                // bool = false;
-                 if (n === 1) {
-                     bool = false;
-                 }
-
-             } else {
-                 await executor.executeTask(task);
-                 //bool = true;
-             }*/
-
-        arrTask.push(task.targetId);
+        if (arrTaskRunning.length <= maxThreads) {
+            if (!arrTaskRunning.includes(arr[n + 1])) {
+                executor.executeTask(task);
+            } else {
+                arrTaskRunning.length = 0;
+                await executor.executeTask(task);
+            }
+        } else {
+            arrTaskRunning.length = 0;
+            await executor.executeTask(task);
+        }
         n++;
-        //arr.shift();
     }
-    // console.log(taskNext.q);
-    console.log(arr);
-
-
 }
