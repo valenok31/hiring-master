09dd98a5a7fc04c174fe4be2c5647db1bdbb18a6
Andrey <priv1984@yandex.ru>
2023-02-22 22:02:00 +0700

    93.94 / 94.44 / 93.33 / very good indeed

--- src/run.ts
+++ src/run.ts
@@ -16,10 +16,10 @@ export default async function run(executor: IExecutor, queue: AsyncIterable<ITas
     const arrTask: any = [];
     const arrTaskRunning: any = [];
     let taskNext = JSON.parse(JSON.stringify(queue));
-    let bool = true;
+
     let n = 0;
     let threads = 0;
-    let set = new Set();
+
 
     function setArr() {
         for (let i of taskNext.q) {
@@ -29,64 +29,87 @@ export default async function run(executor: IExecutor, queue: AsyncIterable<ITas
     }
 
     setArr();
-    for (let j = 0; j < 4; j++) {
-        set.add(arr[j]);
-    }
 
     function exec(t: ITask) {
         executor.executeTask(t).then((r) => {
-            const index = arrTaskRunning.indexOf(t.targetId);
-            if (index !== -1) {
-                arrTaskRunning.splice(index, 1);
-            }
+            spliceArr(arrTaskRunning, t.targetId);
+            /*            const index = arrTaskRunning.indexOf(t.targetId);
+                        if (index !== -1) {
+                            arrTaskRunning.splice(index, 1);
+                        }*/
             return t.targetId;
         })
     }
 
-    async function forGeneral() {
+    await generalFor();
+
+
+    function spliceArr(arrS: any, id: any) {
+        const index = arrS.indexOf(id);
+        if (index !== -1) {
+            arrS.splice(index, 1);
+        }
+    }
+
+
+    async function generalFor() {
+
         for await (let task of queue) {
 
-            arrTaskRunning.push(task.targetId);
+            if (arrTaskRunning.includes(task.targetId)) {
+                n++; // ??????
+                continue;
+            } else {
+                arrTaskRunning.push(task.targetId);
+            }
+
+
             if (arr[n + 1] === undefined || arr[n] === undefined) {
                 await executor.executeTask(task);
-                const index = arrTaskRunning.indexOf(task.targetId);
-                if (index !== -1) {
-                    arrTaskRunning.splice(index, 1);
-                }
+                spliceArr(arrTaskRunning, task.targetId);
+                /*                const index = arrTaskRunning.indexOf(task.targetId);
+                                if (index !== -1) {
+                                    arrTaskRunning.splice(index, 1);
+                                }*/
                 n++;
                 continue;
             }
+
+
             if (arrTaskRunning.length <= maxThreads - 1) {
                 if (!arrTaskRunning.includes(arr[n + 1])) {
                     //executor.executeTask(task);
                     exec(task);
                 } else {
                     //arrTaskRunning.length = 0;
+
                     await executor.executeTask(task);
-                    const index = arrTaskRunning.indexOf(task.targetId);
-                    if (index !== -1) {
-                        arrTaskRunning.splice(index, 1);
-                    }
+                    spliceArr(arrTaskRunning, task.targetId);
+                    /*                    const index = arrTaskRunning.indexOf(task.targetId);
+                                        if (index !== -1) {
+                                            arrTaskRunning.splice(index, 1);
+                                        }*/
                 }
             } else {
                 //arrTaskRunning.length = 0;
                 await executor.executeTask(task);
-                const index = arrTaskRunning.indexOf(task.targetId);
-                if (index !== -1) {
-                    arrTaskRunning.splice(index, 1);
-                }
+                spliceArr(arrTaskRunning, task.targetId);
+                /*                const index = arrTaskRunning.indexOf(task.targetId);
+                                if (index !== -1) {
+                                    arrTaskRunning.splice(index, 1);
+                                }*/
             }
             n++;
 
 
         }
-        console.log(arrTaskRunning)
     }
 
-    forGeneral();
+    console.log(arrTaskRunning)
 
-    if (setArr() > 0) {
-        forGeneral();
-    }
-    //console.log(arr);
+
+/*    if (arrTaskRunning.length>0) {
+         generalFor();
+    }*/
+
 }
