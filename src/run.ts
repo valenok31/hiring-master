b0197b8cecc4535831ad59117c2034073908b34c
Andrey <priv1984@yandex.ru>
2023-02-26 01:35:27 +0700

    poorly :(

--- src/run.ts
+++ src/run.ts
@@ -14,11 +14,13 @@ export default async function run(executor: IExecutor, queue: AsyncIterable<ITas
 
     const arr: any = [];
     const arrTask: any = [];
+    const secondQueue: any = [];
     const arrTaskRunning: any = [];
     let taskNext = JSON.parse(JSON.stringify(queue));
 
     let n = 0;
-    let threads = 0;
+    let threads = true;
+    let taskN_1: ITask;
 
     function setArr() {
         for (let i of taskNext.q) {
@@ -29,6 +31,14 @@ export default async function run(executor: IExecutor, queue: AsyncIterable<ITas
 
     setArr();
 
+    function sleep(milliseconds: any) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+            currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+    }
+
     function exec(t: ITask) {
         executor.executeTask(t).then((r) => {
             spliceArr(arrTaskRunning, t.targetId);
@@ -36,73 +46,123 @@ export default async function run(executor: IExecutor, queue: AsyncIterable<ITas
         })
     }
 
-
     function spliceArr(arrS: any, id: any) {
         const index = arrS.indexOf(id);
         if (index !== -1) {
             arrS.splice(index, 1);
         }
     }
-
+    await generalFor();
 
     async function generalFor() {
 
         for await (let task of queue) {
 
-            if (arrTaskRunning.includes(task.targetId)) {
-                arrTask.push(task);
-                n++;
-                continue;
-            }
+            /*            if (secondQueue.length > 0) {
+                            if (!arrTaskRunning.includes(secondQueue[0].targetId)) {
+                                await executor.executeTask(secondQueue[0]);
+                                secondQueue.shift();
+                            }
+                        }*/
 
-            if (arr[n + 1] === undefined || arr[n] === undefined) {
-                //arrTaskRunning.push(task.targetId);
-                await executor.executeTask(task);
-                //spliceArr(arrTaskRunning, task.targetId);
-                n++;
-                continue;
-            }
 
-            if (arrTaskRunning.length < maxThreads - 1) {
-                if (arrTask.length > 0) {
-                    if (!arrTaskRunning.includes(arrTask[0].targetId)) {
-                        await executor.executeTask(arrTask[0]);
-                        arrTask.shift();
-                    }
-                }
-                /*                if (arrTask.length > 0) {
-                                    if (!arrTaskRunning.includes(arrTask[0].targetId)) {
-                                        await executor.executeTask(arrTask[0]);
-                                        arrTask.shift();
-                                    }
+           // if (arrTaskRunning.includes(task.targetId)) {
+            if (true) {
+                /*                if (taskN_1.targetId == task.targetId) {
+                                    await executor.executeTask(taskN_1);
+                                    spliceArr(arrTaskRunning, taskN_1.targetId);
                                 }*/
-                arrTaskRunning.push(task.targetId);
-                if (!arrTaskRunning.includes(arr[n + 1])) {
-                    exec(task);
-                } else {
-                    if (arrTask.length == 0 || arrTask.length > 3) {
-                        await executor.executeTask(task);
-                        spliceArr(arrTaskRunning, task.targetId);
-                    } else {
-                        exec(task);
-                    }
-
-
-                }
+                secondQueue.push(task);
             } else {
-                await executor.executeTask(task);
+                if (arrTaskRunning.length < maxThreads - 1) {
+                    if (false) {
+                        await executor.executeTask(task);
+                    } else {
+                        arrTaskRunning.push(task.targetId);
+                        exec(task);
+                        //await executor.executeTask(task);
+                        //await sleep(1);
+                    }
+                } else {
+                    await executor.executeTask(task);
+                }
             }
+            taskN_1 = task;
+        }
+
+
+        //if (secondQueue.length > 0) {
+        for await (let task2 of secondQueue) {
+            await executor.executeTask(task2);
+        }
+        // }
+
+
+        /* ===================*/
+
+        /*
+
+        if (arrTaskRunning.includes(task.targetId)) {
+            arrTask.push(task);
             n++;
-        }
-    }
-
-    console.log(arr)
-    await generalFor();
-    if (arrTask.length > 0) {
-        for await(let r of arrTask) {
-            await executor.executeTask(r);
-            arrTask.shift();
+            continue;
         }
 
+        if (arr[n + 1] === undefined || arr[n] === undefined) {
+            //arrTaskRunning.push(task.targetId);
+            await executor.executeTask(task);
+            //spliceArr(arrTaskRunning, task.targetId);
+            n++;
+            continue;
+        }
+
+        if (arrTaskRunning.length < maxThreads - 1) {
+            if (arrTask.length > 0) {
+                if (!arrTaskRunning.includes(arrTask[0].targetId)) {
+                    await executor.executeTask(arrTask[0]);
+                    /!*                        arrTaskRunning.push(arrTask[0].targetId);
+                                            exec(arrTask[0]);*!/
+                    arrTask.shift();
+                }
+            }
+            /!*                if (arrTaskRunning.includes(task.targetId)) {
+                                arrTask.push(task);
+                                n++;
+                                continue;
+                            }*!/
+            /!*                if (arrTask.length > 0) {
+                                if (!arrTaskRunning.includes(arrTask[0].targetId)) {
+                                    await executor.executeTask(arrTask[0]);
+                                    arrTask.shift();
+                                }
+                            }*!/
+            arrTaskRunning.push(task.targetId);
+            if (!arrTaskRunning.includes(arr[n + 1])) {
+                exec(task);
+            } else {
+                if (arrTask.length == 0 || arrTask.length > 3) {
+                    await executor.executeTask(task);
+                    spliceArr(arrTaskRunning, task.targetId);
+                } else {
+                    exec(task);
+                }
+
+
+            }
+        } else {
+            await executor.executeTask(task);
+        }
+        n++;*/
     }
+
+
+    //console.log(arr)
+
+    /*    if (arrTask.length > 0) {
+            for await(let r of arrTask) {
+                await executor.executeTask(r);
+                arrTask.shift();
+            }
+
+        }*/
 }
